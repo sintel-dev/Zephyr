8ea57834b7d87165608dfe44e6e5e11cc03b208f
sarahmish <sarahalnegheimish@gmail.com>
2023-02-15 22:37:23 -0600

    sync version status

--- setup.py
+++ setup.py
@@ -109,6 +109,6 @@ setup(
     test_suite='tests',
     tests_require=tests_require,
     url='https://github.com/sintel-dev/zephyr',
-    version='0.0.0',
+    version='0.0.1.dev0',
     zip_safe=False,
 )
--- zephyr_ml/__init__.py
+++ zephyr_ml/__init__.py
@@ -4,7 +4,7 @@
 
 __author__ = 'MIT Data To AI Lab'
 __email__ = 'dai-lab@mit.edu'
-__version__ = '0.2.4.dev0'
+__version__ = '0.0.1.dev0'
 
 import os
 
