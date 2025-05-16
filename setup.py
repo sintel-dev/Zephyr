2b696e6f84e7a3ba9cc7ba29305975e998dab8fb
Raymond Pan <raymondpan12345@gmail.com>
2025-05-16 15:41:50 -0400

    Bump version: 0.0.5.dev5 → 0.0.5.dev6

--- setup.py
+++ setup.py
@@ -124,6 +124,6 @@ setup(
     test_suite='tests',
     tests_require=tests_require,
     url='https://github.com/sintel-dev/zephyr',
-    version='0.0.5.dev5',
+    version='0.0.5.dev6',
     zip_safe=False,
 )
